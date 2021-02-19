bf82cd3ef098b00d58c5e70f6deca27a1a830450
thiggena <t.iggena@hs-osnabrueck.de>
2021-02-20 00:08:47 +0100

    first version of jokeapp

---
+++ data/src/main/java/de/tanktoo/jokeapp/controllers/JokesController.java
@@ -0,0 +1,24 @@
+package de.tanktoo.jokeapp.controllers;
+
+import de.tanktoo.jokeapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+
+    private final JokeService jokeService;
+
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String getJoke(Model model){
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "joke";
+    }
+}
---
+++ data/src/main/java/de/tanktoo/jokeapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package de.tanktoo.jokeapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
